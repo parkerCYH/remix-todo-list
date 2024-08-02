a763770ed7790eb510711a1e18364450b713aaa1
parker cheng <fbi0258zzz@gmail.com>
2024-08-02 19:29:54 +0800

    prettier, eslint

---
+++ .prettierrc.cjs
@@ -0,0 +1,12 @@
+/**
+ * @type {import("prettier").Config}
+ */
+module.exports = {
+  trailingComma: 'none',
+  singleQuote: true,
+  tabWidth: 2,
+  printWidth: 100,
+  arrowParens: 'always',
+  semi: true,
+  plugins: ['prettier-plugin-tailwindcss']
+};
--- app/root.tsx
+++ app/root.tsx
@@ -1,45 +1,33 @@
-import type { LinksFunction } from "@remix-run/node";
-import {
-  Link,
-  Links,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
-import { NavigationMenuDemo } from "./components/nav";
-import stylesheet from "./globals.css?url";
+import type { LinksFunction } from '@remix-run/node';
+import { Link, Links, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
+import { NavigationMenuDemo } from './components/nav';
+import stylesheet from './globals.css?url';
 
-export const links: LinksFunction = () => [
-  { rel: "stylesheet", href: stylesheet },
-];
+export const links: LinksFunction = () => [{ rel: 'stylesheet', href: stylesheet }];
 
-export function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <header>
-          <div className="flex items-center p-9 md:gap-16">
-            <Link className="text-xl font-bold sm:text-3xl" to="/">
-              Remix Todo List
-            </Link>
-            <NavigationMenuDemo />
-          </div>
-        </header>
-        {children}
-        <ScrollRestoration />
-        <Scripts />
-      </body>
-    </html>
-  );
-}
+export const Layout = ({ children }: { children: React.ReactNode }) => (
+  <html lang="en">
+    <head>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <Meta />
+      <Links />
+    </head>
+    <body>
+      <header>
+        <div className="flex items-center p-9 md:gap-16">
+          <Link className="text-xl font-bold sm:text-3xl" to="/">
+            Remix Todo List
+          </Link>
+          <NavigationMenuDemo />
+        </div>
+      </header>
+      {children}
+      <ScrollRestoration />
+      <Scripts />
+    </body>
+  </html>
+);
 
-export default function App() {
-  return <Outlet />;
-}
+const App = () => <Outlet />;
+export default App;
